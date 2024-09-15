aecfdc5a80139683c5f4f962e3c52532611cef8e
nikit-nikolaev <167652738+nikit-nikolaev@users.noreply.github.com>
2024-09-16 01:41:24 +0300

    Update calculation.py
    не много подправил код

--- calculation.py
+++ calculation.py
@@ -7,13 +7,9 @@ def price_BTC_usd():
     price = f"{(data['bitcoin']['usd']):,d}"
     return (price)
 
-print(price_BTC_usd())
-
 def price_BTC_rub():
     url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=rub'
     response = requests.get(url)
     data = response.json()
     price = f"{(data['bitcoin']['rub']):,d}"
     return (price)
-
-print(price_BTC_rub())
